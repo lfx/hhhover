d1ddb3876e25394101a3c7a14817713f939159da
Liudas Sodonis <liudas@sodonis.lt>
2015-10-02 07:02:39 -0700

    Fixes #1

--- hhhover.js
+++ hhhover.js
@@ -2,7 +2,7 @@ function loadJQ() {
     if (typeof jQuery == 'undefined') {
         var script = document.createElement('script');
         script.type = "text/javascript";
-        script.src = "http://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js";
+        script.src = "//ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js";
         document.getElementsByTagName('head')[0].appendChild(script);
     }
 }
